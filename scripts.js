eee7c34bd38c109b876bc9dd7b859c59860ebab5
Ambrosius <ambrosius411@protonmail.com>
2023-04-18 14:25:40 +0200

    Add functionality to keep score

--- scripts.js
+++ scripts.js
@@ -1,38 +1,63 @@
 function createPlayer(name, sign) {
   const playerName = name;
   const playerSign = sign;
+  let playerScore = 0;
+
   return {
     getName: () => playerName,
     getSign: () => playerSign,
+    getPlayerScore: () => playerScore,
+    addPoint: () => playerScore++,
   };
 }
 
-const player1 = createPlayer("Bart", "X");
-const player2 = createPlayer("Yvoine", "O");
-
 const gameboard = (function () {
   let board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  const players = [player1, player2];
+  const players = [];
   let activePlayer = players[0];
 
   function _changeActivePlayer() {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    displayController.updateActivePlayer(activePlayer);
   }
 
-  function _resetGame() {
+  function newGame(player1Name, player2Name) {
+    players.length = 0;
+    const player1 = createPlayer(player1Name, "X");
+    const player2 = createPlayer(player2Name, "O");
+    players.push(player2);
+    players.push(player1);
+    _changeActivePlayer();
+    displayController.updateScoreboard(
+      players[0].getPlayerScore(),
+      players[1].getPlayerScore()
+    );
+    resetRound();
+  }
+
+  function resetRound() {
     board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    displayController.render();
+    displayController.renderGameBoard();
+    displayController.updateActivePlayer(activePlayer);
   }
 
   function _declareWinner(winner = "tie") {
     setTimeout(() => {
       if (winner === "tie") {
-        alert("It's a tie!");
+        displayController.endRoundMessage("It's a tie!");
       } else {
-        alert(`${winner.getName()} is the winner!`);
+        activePlayer.addPoint();
+        displayController.endRoundMessage(`${winner.getName()} is the winner!`);
+        console.log(
+          `Player1: ${players[0].getPlayerScore()}, Player2: ${players[1].getPlayerScore()}`
+        );
+        displayController.updateScoreboard(
+          players[0].getPlayerScore(),
+          players[1].getPlayerScore()
+        );
       }
-      _resetGame();
-    }, 10); // Delay the execution of _resetGame() by 10 milliseconds)
+      resetRound();
+    }, 10); // Delay the execution of _resetRound() by 10 milliseconds)
   }
 
   function _determineGameOver() {
@@ -63,7 +88,7 @@ const gameboard = (function () {
     const id = event.target.getAttribute("data-id");
     if (board[id] !== 0) return;
     gameboard.addToGameBoard(id, activePlayer.getSign());
-    displayController.render();
+    displayController.renderGameBoard();
     _determineGameOver();
     _changeActivePlayer();
   }
@@ -75,13 +100,46 @@ const gameboard = (function () {
     board[index] = value;
   }
 
-  return { getGameBoard, addToGameBoard, playRound };
+  return { getGameBoard, addToGameBoard, playRound, resetRound, newGame };
 })();
 
 const displayController = (function () {
   const gameBoardContainer = document.getElementById("gameBoard");
+  const inputModal = document.getElementById("playerInputModal");
+  const closeButton = inputModal.querySelector(".close");
+  const startButton = document.getElementById("startGame");
+  const resetButton = document.getElementById("resetGame");
+  const submitButton = document.getElementById("submitNames");
+  const messageDisplay = document.getElementById("endRoundMessage");
+  const currentPlayerDisplay = document.getElementById("currentPlayer");
+  const player1Display = document.getElementById("player1Name");
+  const player2Display = document.getElementById("player2Name");
+  const player1NameDisplay = document.getElementById("player1");
+  const player2NameDisplay = document.getElementById("player2");
+  const player1Score = document.getElementById("player1Score");
+  const player2Score = document.getElementById("player2Score");
 
-  function render() {
+  closeButton.addEventListener("click", () => {
+    inputModal.classList.add("hidden");
+  });
+
+  startButton.addEventListener("click", () => {
+    inputModal.classList.remove("hidden");
+  });
+
+  resetButton.addEventListener("click", () => {
+    gameboard.resetRound();
+  });
+
+  submitButton.addEventListener("click", () => {
+    inputModal.classList.add("hidden");
+    const player1Name = player1NameDisplay.value;
+    const player2Name = player2NameDisplay.value;
+    gameboard.newGame(player1Name, player2Name);
+    _updateNameDisplay(player1Name, player2Name);
+  });
+
+  function renderGameBoard() {
     const gameBoardArray = gameboard.getGameBoard();
     gameBoardContainer.innerHTML = "";
     for (let i = 0; i < gameBoardArray.length; i++) {
@@ -94,7 +152,36 @@ const displayController = (function () {
       gameBoardContainer.appendChild(newDiv);
     }
   }
-  return { render };
-})();
 
-displayController.render();
+  function _updateNameDisplay(name1, name2) {
+    player1Display.innerText = `${name1} (X)`;
+    player2Display.innerText = `${name2} (O)`;
+  }
+
+  function updateActivePlayer(activePlayer) {
+    currentPlayerDisplay.innerText = `${activePlayer.getName()} (${activePlayer.getSign()} )`;
+  }
+
+  function endRoundMessage(text) {
+    messageDisplay.innerText = text;
+    messageDisplay.classList.remove("hidden");
+    setTimeout(() => {
+      messageDisplay.classList.add("hidden");
+    }, 3000);
+  }
+
+  function updateScoreboard(score1, score2) {
+    player1Score.innerText = score1;
+    player2Score.innerText = score2;
+  }
+
+  return {
+    renderGameBoard,
+    updateActivePlayer,
+    endRoundMessage,
+    updateScoreboard,
+  };
+})();
+gameboard.newGame("Player1", "Player2");
+
+displayController.renderGameBoard();
